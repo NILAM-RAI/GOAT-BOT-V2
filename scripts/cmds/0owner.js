c4d681a324a04ecb1c0a63aa24908284085f911a
NILAM <nilamrai82206@gmail.com>
2025-05-15 13:19:28 +0530

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -24,7 +24,7 @@ config: {
       nick: 'neha'
     };
 
-    const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://imgur.com/gallery/sierpinski-triangle-loop-w-chromatic-aberration-PfvgkHj'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
